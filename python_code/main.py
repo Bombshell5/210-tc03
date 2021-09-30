e4bb08b6511e7332fef551d4b49f2d409bb27297
Bombshell5 <90216645+Bombshell5@users.noreply.github.com>
2021-09-30 14:12:04 -0600

    Update main.py

--- python_code/main.py
+++ python_code/main.py
@@ -1,4 +1,6 @@
 from wilding import print_greeting_wilding
+from vega import display_Shannon_greeting
 
 print("Welcome to our collaborative program.")
 print_greeting_wilding()
+display_Shannon_greeting()
