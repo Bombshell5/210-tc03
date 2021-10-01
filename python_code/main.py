24c1fe5eeb26ebe25f548fe4d5e04e2c9ae6beee
Jonathan <jlgunderson2@gmail.com>
2021-10-01 14:11:31 -0600

    Joanthan Gunderson added code

--- python_code/main.py
+++ python_code/main.py
@@ -1,6 +1,8 @@
 from wilding import print_greeting_wilding
 from vega import display_Shannon_greeting
+from jonathan import print_greeting_jonathan
 
 print("Welcome to our collaborative program.")
 print_greeting_wilding()
 display_Shannon_greeting()
+print_greeting_jonathan()
